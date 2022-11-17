60b531731ec4b3498890692d6d746f4789a86fa1
Denis Shalaev <pomarki@narod.ru>
2022-11-17 13:45:51 +0300

    themes toggle

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,16 @@
 const themesButton = document.getElementById("themes-button");
+const langButtons = Array.from(
+  document.querySelectorAll(".header__language-selector")
+);
 const pageBody = document.querySelector(".body");
 const sunBlock = document.querySelector(".header__themes-indicator_sun_light");
-const moonBlock = document.querySelector(".header__themes-indicator_moon_light");
+const moonBlock = document.querySelector(
+  ".header__themes-indicator_moon_light"
+);
+const title = document.querySelector(".header__title");
+const portfolioLinks = Array.from(
+  document.querySelectorAll(".portfolio__site_name")
+);
+const avatar = document.querySelector(".header__avatar");
 
-export { themesButton, pageBody, sunBlock, moonBlock };
+export { themesButton, langButtons, pageBody, sunBlock, moonBlock, title, portfolioLinks, avatar };
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,32 @@
-import { themesButton, pageBody, sunBlock, moonBlock } from "./data.js";
+import {
+  themesButton,
+  langButtons,
+  pageBody,
+  sunBlock,
+  moonBlock,
+  title,
+  portfolioLinks,
+  avatar,
+} from "./data.js";
+
+let mainTheme = true;
 
 const themesToggle = () => {
+  mainTheme = !mainTheme;
   pageBody.classList.toggle("body_theme_dark");
   themesButton.classList.toggle("header__themes-button_dark");
   sunBlock.classList.toggle("header__themes-indicator_sun_dark");
   moonBlock.classList.toggle("header__themes-indicator_moon_dark");
+  title.classList.toggle("header__title_theme_dark");
+  langButtons.forEach((el) => {
+    el.classList.toggle("header__language-selector_theme_dark");
+  });
+  portfolioLinks.forEach((el) => {
+    el.classList.toggle("portfolio__site_name_theme_dark");
+  });
+  mainTheme
+    ? (avatar.src = "images/face__light.jpg")
+    : (avatar.src = "images/face__dark.jpg");
 };
 
 themesButton.addEventListener("click", themesToggle);
